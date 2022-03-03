2f88c4e6069d7a284e972e9e1de4e49325e7bec1
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-03 15:20:57 +0100

    Created Jumbtron

--- src/components/header/NavBar.vue
+++ src/components/header/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
- <nav class="position-absolute d-flex align-items-center justify-content-between">
+ <nav class="d-flex align-items-center justify-content-between">
    <img :src="require ('../../assets/images/medical_logo_1x_light.png')" alt="Medical Logo">
    <ul class="d-flex list-unstyled text-uppercase">
     <li v-for="(info,index) in dataNav" :key="index">
       <a 
-        :class="`p-3 text-decoration-none ${info.active ? 'text-primary' : 'text-white'}`" 
+        :class="`p-3 text-decoration-none ${info.active ? 'text-info' : 'text-white'}`" 
         :href="info.url">{{info.name}}
       </a>
     </li>
@@ -47,8 +47,7 @@ export default {
 
 <style scoped lang="scss">
   nav{
-    top: 0;
-    left: 2.5%;
+    margin: auto;
     height: 80px;
     width: 95%;
     .my_button{
--- src/components/header/ThumbHeader.vue
+++ src/components/header/ThumbHeader.vue
@@ -1,15 +1,25 @@
 <template>
- <div class="thumb">
+ <div class="thumb position-relative" :style="bgThumb">
    <NavBar />
-   <img :src="require ('../../assets/images/header-image-homepage.jpg')" alt="">
+   <div class="content-thumb text-white">
+     <h1 class="mb-3">Caring <span class="text-info">For Life</span></h1>
+     <p>Kind words can be short and easy to speak, but their echoes ar truly endless. Avada Health focuses on you as if it was our own family.</p>
+     <div class="mt-3 text-uppercase my_button bg-info rounded d-inline-block">Learn More</div>
+   </div>
  </div>
 </template>
 
 <script>
   import NavBar from "./NavBar.vue"
-
   export default {
     name: 'ThumbHeader',
+    data(){
+      return{
+        bgThumb: {
+          backgroundImage: `url(${require('../../assets/images/header-image-homepage.jpg')})`
+        }
+      }
+    },
     props: {
     },
     components: {
@@ -20,13 +30,24 @@
 
 <style scoped lang="scss">
   .thumb{
-    position: relative;
-    height: 700px;
+    height: 650px;
+    background-position: top;
+    background-size: cover;
     img{
       width: 100%;
       height: 100%;
       object-fit: cover;
-      object-position: top;
+    }
+    .content-thumb{
+      width: 35%;
+      margin: 150px 0 0 200px;
+      h1{
+        font-size: 55px;
+        font-weight: 200;
+      }
+      .my_button{
+      padding: 12px 35px;
+      }
     }
   }
 </style>
